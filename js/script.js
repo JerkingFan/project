97949ad3305336c3d6ba0c6fc97424c4b088710d
Arseny <example@gmail.com>
2024-07-24 11:28:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use sctrict"
+
+const obj = {
+
+    name: "John",
+    age: 25,
+    isMarried: false
+    
+};
+
+console.log(obj.age);
+console.log(obj["name"]);
+
+let array = ['s.png', 'ora.png', 3, false, obj]
+
+console.log(array[array.length-2]);
