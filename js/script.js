bcf0e911a33fa4d22854f413888aa57e5cf1c820
Arseny <example@gmail.com>
2024-07-24 12:27:28 +0300

    refactor cycles

--- js/script.js
+++ js/script.js
@@ -12,18 +12,29 @@ let personalMovieDB = {
 
 }
 
-let i = 0;
-
-let movies = {
-
-
-
-
+if (personalMovieDB.count < 0){
+    alert("Произошла ошибка");
 }
+else if (personalMovieDB.console >= 0 && personalMovieDB.count < 10){
+    alert("Слишком мало фильмов");
+}
+else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    alert("Вы классический зритель");
+}
+else{
+    alert("Вы киноман");
+}
+
+let i = 0;
 
 while (i< personalMovieDB.count ){
 
     let film = prompt("Один из последниъ прсомотренных фильмо?", 0);
+    if (film.length == 0 || film.length > 50){
+        prompt("Произошло что-то не так");
+        continue;
+        i--;
+    }
     let degree = prompt("На сколько оцените его?", 0);
 
     personalMovieDB.movies[film] = degree;
